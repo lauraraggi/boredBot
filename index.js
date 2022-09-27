197577bfa68a4e9856889f4859f22a50b42fe169
Laura Raggi <raggiorama@gmail.com>
2022-09-27 11:31:18 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container");
+fetch("https://apis.scrimba.com/bored/api/activity")
+  .then((response) => response.json())
+  .then(({ activity, type, participants, price, link, key, accessibility }) => {
+    container.innerHTML = `<div id="${key}" class="activity">
+    
+  <div class="title">
+    <h2 class="activity__title">${activity}</h2>
+    
+  </div>
+  <div class="info">
+    <div class="activity__type">${type}</div>
+    <div class="participants">Participants: ${participants}</div>
+    <div class="price">Price: €${Math.round(price)}</div>
+  </div>
+</div>`;
+  });
